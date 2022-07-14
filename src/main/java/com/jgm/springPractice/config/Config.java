06e0c62892e3273ef31f4f6e2aeb6329602e0251
jgm0327 <gmjang6349@gmail.com>
2022-07-14 19:28:53 +0900

    Config #1 - spring configuration
    기초 설정
    루트 패키지 이하에 빈이 있는지 스캔한다.

---
+++ src/main/java/com/jgm/springPractice/config/Config.java
@@ -0,0 +1,13 @@
+package com.jgm.springPractice.config;
+
+import com.jgm.springPractice.logic.BubbleSort;
+import com.jgm.springPractice.logic.service.SortService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.jgm.springPractice")
+@Configuration
+public class Config {
+
+}
